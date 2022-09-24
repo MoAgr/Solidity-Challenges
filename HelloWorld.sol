af414ba2803a9289d0e8b866e0601b2694c70b00
Remix Workspace <>
2022-09-24 23:42:40 +0545

    Number 1 Completed

---
+++ HelloWorld.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+contract HelloWorld{
+    uint num;
+
+    function storeNumber(uint _num)public{
+        num=_num;
+    }
+    function retrieveNumber()public view returns(uint){
+        return num;
+    }
+}
